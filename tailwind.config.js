5ff9b9af3d91e27eb7fbce284d2b748cc224bebc
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-01-31 12:19:53 +0100

    Fix dependency issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./**/*.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./**/*.html'],
   theme: {
     fontFamily: {
       serif: ['Merriweather', 'serif'],
